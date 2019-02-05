b0bddaa43535ba4145b381ef0830f5100487649d
Jake Rickard <jakelrickard@gmail.com>
2019-02-05 03:41:22 +0000

    favorite sports player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,20 +1,14 @@
-class HomeController < ApplicationController
-
-  def index
-    @player = Player.new
-    @players = Player.all  
-  end
+class PlayersController < ApplicationController
 
   def create
     @player = Player.create(player_params)
     redirect_to root_path
   end
 
-
   private
 
   def player_params
     params.require(:player).permit(:name, :position, :team)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'player/create'
   root 'home#index'
+  resources :home
+  resources :players
 end
